5c961cc4ef5e6f069d2d226fcb363eac9945da19
Cyprian Kinyua <132161346+Cyprian-Kinyua@users.noreply.github.com>
2025-05-18 20:44:39 +0300

    Create perfomance.sql
    Implementing Query Optimization

---
+++ database-adv-script/perfomance.sql
@@ -0,0 +1,42 @@
+# Complex Query before optimization
+SELECT 
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    b.status AS booking_status,
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    p.property_id,
+    p.name AS property_name,
+    p.location,
+    pay.payment_id,
+    pay.amount,
+    pay.payment_method,
+    pay.payment_date
+FROM Booking b
+JOIN user u ON b.user_id = u.user_id
+JOIN property p ON b.property_id = p.property_id
+LEFT JOIN Payment pay ON b.booking_id = pay.booking_id;
+
+# Analyzing Performance
+EXPLAIN SELECT ...
+
+# Optimized Query
+SELECT 
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    b.status,
+    u.first_name,
+    u.last_name,
+    p.name AS property_name,
+    pay.amount,
+    pay.payment_method
+FROM Booking b
+JOIN user u ON b.user_id = u.user_id
+JOIN property p ON b.property_id = p.property_id
+LEFT JOIN Payment pay ON b.booking_id = pay.booking_id;
