6205742137fea8ee28b95ec1c3574267d2972b49
Cyprian Kinyua <132161346+Cyprian-Kinyua@users.noreply.github.com>
2025-05-18 20:05:26 +0300

    Create database_index.sql
    Implemented Indexes for high usage columns critical tables including: User, Property, Booking, and Review.

---
+++ database-adv-script/database_index.sql
@@ -0,0 +1,15 @@
+# Indexes on user table
+CREATE INDEX idx_user_email ON user(email);
+CREATE INDEX idx_user_userid ON user(user_id);
+
+# Indexes on property table
+CREATE INDEX idx_property_host ON property(host_id);
+CREATE INDEX idx_property_location ON property(location);
+
+# Indexes on booking table
+CREATE INDEX idx_booking_user ON Booking(user_id);
+CREATE INDEX idx_booking_property ON Booking(property_id);
+CREATE INDEX idx_booking_startdate ON Booking(start_date);
+
+# Index on review table
+CREATE INDEX idx_review_property ON Review(property_id);
