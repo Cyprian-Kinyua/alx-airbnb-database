d9047f214a143f6844b41af55ec07eb8cbc9603b
Cyprian Kinyua <132161346+Cyprian-Kinyua@users.noreply.github.com>
2025-05-18 19:38:43 +0300

    Create subqueries.sql
    Implementing correlated and non-correlated subqueries (Nested Queries)

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,18 @@
+# Non-Correlated Subquery to find all properties where the average rating is greater than 4.0 
+SELECT *
+FROM property
+WHERE property_id IN (
+    SELECT property_id
+    FROM Review
+    GROUP BY property_id
+    HAVING AVG(rating) > 4.0
+);
+
+# correlated subquery to find users who have made more than 3 bookings.
+  SELECT *
+FROM user u
+WHERE (
+    SELECT COUNT(*)
+    FROM Booking b
+    WHERE b.user_id = u.user_id
+) > 3;
